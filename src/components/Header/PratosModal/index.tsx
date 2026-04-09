21e88c8d90e8c214f55f181d55453cec478ca027
kauan <kauan@teste.com>
2026-04-09 15:25:16 -0300

    Adicionado a capacidade de botar itens no carrinho

--- src/components/Header/PratosModal/index.tsx
+++ src/components/Header/PratosModal/index.tsx
@@ -2,6 +2,9 @@ import * as S from './styles'
 import fechar from '../../../assets/images/close 1.png'
 import { InfoPrato } from '../PratosList'
 import { formataPreco } from '../../../models/formatters'
+import { useDispatch } from 'react-redux'
+import { add, open } from '../../../store/reducers/cart'
+import { toast } from 'react-toastify'
 
 type Props = {
   product: InfoPrato | null
@@ -10,6 +13,24 @@ type Props = {
 }
 
 const ProductModal = ({ product, isVisible, onClose }: Props) => {
+  const dispatch = useDispatch()
+
+  const addItem = () => {
+    if (product) {
+      dispatch(add(product))
+      onClose()
+
+      toast.success('Item adicionao! Ver carrinho', {
+        onClick: () => dispatch(open()),
+        icon: <span>🛒</span>,
+        position: 'bottom-right',
+        autoClose: 4000,
+        pauseOnHover: true,
+        draggable: true
+      })
+    }
+  }
+
   if (!isVisible || !product) return null
 
   return (
@@ -25,7 +46,7 @@ const ProductModal = ({ product, isVisible, onClose }: Props) => {
             <p>
               Serve de <span>{product.porcao}</span>
             </p>
-            <S.BotaoModal>
+            <S.BotaoModal onClick={addItem}>
               Adicionar ao carrinho - {formataPreco(product.preco)}
             </S.BotaoModal>
           </div>
